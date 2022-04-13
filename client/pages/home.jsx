0fa7eabd65a81bb1a7712eb4b34d1ed52fd8adb4
Scott Bowler <scott.bowler@learningfuze.com>
2022-04-13 23:16:41 +0000

    Finished Feature 1, user can view entries

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 
-export default function Home(props) {
-  return (
-    <>
-    </>
-  );
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      entries: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/entries')
+      .then(resp => resp.json())
+      .then(entries => {
+        this.setState({ entries });
+      });
+  }
+
+  render() {
+    return (
+      <div className="py-4">
+        <h1>Entries</h1>
+        <div className="row">
+          {
+            this.state.entries.map(entry => (
+              <div className="col-4" key={entry.entryId}>
+                <div className="card">
+                  <img className="card-img-top" src={entry.photoUrl} alt={entry.title} />
+                  <div className="card-body">
+                    <h5 className="card-title">{entry.title}</h5>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
 }
--- server/index.js
+++ server/index.js
@@ -2,10 +2,35 @@ require('dotenv/config');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
+const db = require('./db');
 
 const app = express();
 
 app.use(staticMiddleware);
+
+app.get('/api/entries', (req, res, next) => {
+  const userId = 1;
+
+  const sql = `
+    select
+      "entryId",
+      "title",
+      "photoUrl"
+    from
+      "entries"
+    where
+      "userId" = $1;
+  `;
+  const params = [userId];
+
+  db.query(sql, params)
+    .then(results => {
+      const entries = results.rows;
+
+      res.json(entries);
+    })
+    .catch(err => next(err));
+});
 
 app.use(errorMiddleware);
 
